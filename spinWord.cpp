ab153b6f1f1922b5c566bf5e5284a7a3e58f1a55
Alexey Kryga <48469412+Gornalas@users.noreply.github.com>
2019-10-04 14:03:40 +0300

    Add files via upload

---
+++ spinWord.cpp
@@ -0,0 +1,49 @@
+std::string spinWords(const std::string &str)
+{
+    std::string lookSpace, word, reversed;
+    int lenStr = str.size() - 1;
+    int globalIndex;
+    
+    for (globalIndex = 0; globalIndex <= lenStr; ++globalIndex){			//Start checking all string
+        lookSpace = str[globalIndex];										
+        if (lookSpace == " " || globalIndex == lenStr){
+        	if (globalIndex != lenStr){										//Need plus last letter
+            	int lenWord = word.size();
+           		if (lenWord > 4){											//Reverse word if it has 5 letter or more
+           			int index;      		
+                	for (index = 1; index <= lenWord; ++index){
+                		int indexWord = lenWord - index;
+                		reversed += word[indexWord];
+                	}
+            		word.clear();
+            	}
+            	else{
+            		reversed += word;										//If word less than 5 letter
+            		word.clear();
+            	}
+            reversed += lookSpace;											//Add space at the end of reverse 
+        	}
+        	else{
+        		word += lookSpace;											//Add last letter to word.
+        		int lenWord = word.size();
+           		if (lenWord > 4)
+				{
+           			int index;      		
+                	for (index = 1; index <= lenWord; ++index){
+                		int indexWord = lenWord - index;
+                		reversed += word[indexWord];
+                	}
+            	word.clear();
+            	}
+            	else{
+            		reversed += word;
+            		word.clear();
+            	}
+        	}
+        }
+        else{
+        	word += str[globalIndex];										//If lookSpase has a letter and it isn't last
+        }
+    }
+	  return reversed;
+}// spinWords
