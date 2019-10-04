6aa8dc0be55e23a5b3f9945c2b2b97432736df82
Alexey Kryga <48469412+Gornalas@users.noreply.github.com>
2019-10-04 14:04:42 +0300

    Update spinWord.cpp

--- spinWord.cpp
+++ spinWord.cpp
@@ -1,3 +1,9 @@
+//Write a function that takes in a string of one or more words, 
+//and returns the same string, but with all five or more letter words reversed 
+//(Just like the name of this Kata). Strings passed in will consist of only letters and spaces. 
+//Spaces will be included only when more than one word is present.
+
+
 std::string spinWords(const std::string &str)
 {
     std::string lookSpace, word, reversed;
