e4181e3358a710ef614d33854482ddd82d095d54
Alexey Kryga <48469412+Gornalas@users.noreply.github.com>
2019-11-01 15:15:51 +0300

    You're a square!

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace localin{
+public class Class1
+{
+        static void Main(){
+            double n = 3;
+            if(n < 0){
+                Console.WriteLine("false");
+            }
+            double temp = Math.Pow(n,2);
+            if(Convert.ToInt32(temp) == Convert.ToDouble(temp))
+            {
+                Console.WriteLine("true");
+            }
+            else
+                Console.WriteLine("false");
+        }
+    }
+}
