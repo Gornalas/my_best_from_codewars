1fc02cd50d174401ddcd22ffc6c667279175e6b3
Alexey Kryga <48469412+Gornalas@users.noreply.github.com>
2019-11-06 12:36:53 +0300

    Duplicate Encoder
    https://www.codewars.com/kata/reviews/57ab76bdd9af9cfeed000111/groups/5dc28f504814bd000145603a

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+public class Kata
+{
+  public static string DuplicateEncode(string word)
+  {
+    string newWord = "";
+    string letters = word.ToLower();
+    
+    foreach(char index in letters)
+    {
+      if(letters.Where(ch => ch == index).Count() > 1)
+        newWord += ")";
+      else
+        newWord += "(";    
+    }
+    return newWord;
+  }
+}
