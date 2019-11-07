3df02f1b18d7796659cb22c14d3f1596b58f30e3
Alexey Kryga <48469412+Gornalas@users.noreply.github.com>
2019-11-07 12:27:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Solution
+{
+    class Kata
+    {
+        public static int find_it(int[] seq) 
+        {
+            foreach(int index in seq)
+            {
+                if(seq.Where(ch => ch == index).Count() % 2 == 1)
+                return index;
+            }
+        return -1;
+        }   
+    }
+}
